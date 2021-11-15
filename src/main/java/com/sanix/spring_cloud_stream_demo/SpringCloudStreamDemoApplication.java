f03953edbfefb2cd50a8b66c31688575f9e09957
sanix <sanicksikani@gmail.com>
2021-11-16 02:13:58 +0300

    cloud stream

---
+++ src/main/java/com/sanix/spring_cloud_stream_demo/SpringCloudStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sanix.spring_cloud_stream_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanix/spring_cloud_stream_demo/SpringCloudStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanix.spring_cloud_stream_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
